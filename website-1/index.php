078dfa98b38bd7d3ccd9176c1f9bb3efebe46e26
java <javaplanet17@gmail.com>
2017-08-30 22:29:02 -0400

    -A

---
+++ website-1/index.php
@@ -0,0 +1,173 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>this is a test</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class="content-1">
+	<ul class="navbar">
+		<li><img src="icon.png" width="50" height="50"></li>
+		<li>someting</li>
+		<li>something</li>
+		<li>something</li>
+		<li>something</li>
+	</ul>
+
+	<div class="nav-left">
+		<img src="nav-left-icon.png" width="50" height="50">
+		<h2>there is something here</h2>
+	</div>
+
+	<div class="nav-right-1">
+		<img src="nav-right-1-icon.png" width="50" height="50">
+		<h2>there is something here</h2>
+	</div>
+
+	<div class="nav-right-2">
+		<img src="nav-right-2-icon.png" width="50" height="50">
+		<h2>there is something here</h2>
+	</div>
+
+
+</div>
+
+<div class="content-2">
+	<div class="content-title">
+		<h1>Comment something?</h1>
+		<p>__</p>
+		<p>there is something in here but i didnt know what i want to write so i just type some random word but its all about the design, but i am just showing you the design</p>
+	</div>
+<div class="content-border"> 
+	<div class="content-2-box-1">
+		<h1 class="content-2-number">01</h1>
+		<h1 class="content-2-title">The title </h1>
+		<p class="content-2-content">a little content inside the box.</p>
+	</div>
+
+	<div class="content-2-box-2">
+		<h1 class="content-2-number">01</h1>
+		<h1 class="content-2-title">The title </h1>
+		<p class="content-2-content">a little content inside the box.</p>
+	</div>
+
+	<div class="content-2-box-3">
+		<h1 class="content-2-number">01</h1>
+		<h1 class="content-2-title">The title </h1>
+		
+	</div>
+
+	<div class="content-2-box-4">
+		<h1 class="content-2-number">01</h1>
+		<h1 class="content-2-title">The title </h1>
+	
+	</div>
+
+	<div class="content-2-box-5">
+		<h1 class="content-2-number">01</h1>
+		<h1 class="content-2-title">The title </h1>
+		<p class="content-2-content">a little content inside the box.</p>
+	</div>
+
+	<div class="content-2-box-6">
+		<h1 class="content-2-number">01</h1>
+		<h1 class="content-2-title">The title </h1>
+		<p class="content-2-content">a little content inside the box.</p>
+	</div>
+</div>
+</div>
+
+<div class="content-3">
+	<div class="content-title">
+		<h1>Comment something?</h1>
+		<p>__</p>
+		<p>there is something in here but i didnt know what i want to write so i just type some random word but its all about the design, but i am just showing you the design</p>
+	</div>
+
+
+		<div class="content-border">
+			<div class="content-3-square">
+				<img src="content-3-icon.png" width="40" height="40">
+				<h1 class="content-3-title-1">Economies</h1>
+				<p>some content inside here you can puit wharter aofbofn sd gnlskd gnsldjg nsldg sd,g sdm,g nsd, gnsd, gns,d gnsdl hgosd hlsd ghosd hisd gsd g.</p>
+			</div>
+
+
+	
+			<div class="content-3-square">
+				<img src="content-3-icon.png" width="40" height="40">
+				<h1 class="content-3-title-2">Gain</h1>
+				<p>some content inside here you can puit wharter aofbofn sd gnlskd gnsldjg nsldg sd,g sdm,g nsd, gnsd, gns,d gnsdl hgosd hlsd ghosd hisd gsd g.</p>
+			</div>
+		
+
+		
+			<div class="content-3-square">
+				<img src="content-3-icon.png" width="40" height="40">
+				<h1 class="content-3-title-3">Collaboration</h1>
+				<p>some content inside here you can puit wharter aofbofn sd gnlskd gnsldjg nsldg sd,g sdm,g nsd, gnsd, gns,d gnsdl hgosd hlsd ghosd hisd gsd g.</p>
+			</div>
+		</div>
+</div>
+
+<div class="content-4">
+	<div class="content-title">
+		<h1>Comment something?</h1>
+		<p>__</p>
+		<p>there is something in here but i didnt know what i want to write so i just type some random word but its all about the design, but i am just showing you the design</p>
+	</div>
+
+</div>
+
+<div class="content-5">
+	<div class="content-title">
+		<h1>Comment something?</h1>
+		<p>__</p>
+	</div>
+	
+	<div class="content-border">
+	<ul class="content-5-box-list">
+		<li>
+			<img src="google.png" width="50" height="50">
+		</li>
+		<li>
+			<img src="google.png" width="50" height="50">
+		</li>
+		<li>
+			<img src="google.png" width="50" height="50">
+		</li>
+		<li>
+			<img src="google.png" width="50" height="50">
+		</li>
+		<li>
+			<img src="google.png" width="50" height="50">
+		</li>
+		<li>
+			<img src="google.png" width="50" height="50">
+		</li>
+		<li>
+			<img src="google.png" width="50" height="50">
+		</li>
+		<li>
+			<img src="google.png" width="50" height="50">
+		</li>
+	</ul>
+	</div>
+</div>
+
+<div class="content-6">
+	<form action='contact.php' method='post'>
+		<input class="contact-box-normal" type="box" name="email" placeholder="email here"><br>
+		<input class="contact-box-normal" type="box" name="topic" placeholder="submit topic"><br>
+		<input class="contact-box-large" type="box" name="content" placeholder="comment here"><br>
+		<input class="contact-box-button" type="submit" name="submit" value="Send">
+	</form>
+</div>
+
+<script type="text/javascript" src="">
+	
+</script>
+</body>
+</html>
