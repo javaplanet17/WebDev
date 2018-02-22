098eeafe132b1d4f3d2082c28867197750a3c2d5
Alan Dewo <alan@Alans-MacBook-Air.local>
2018-02-22 14:06:03 -0500

    additional website folder

---
+++ website-2/index.php
@@ -0,0 +1,148 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mattress Store</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+
+</head>
+<body>
+	<div class="content-1">
+		<div class="navbar" id="navbar">
+			<ul class="navbar-left">
+				<li>icon</li>
+				<li>Menu</li>
+			</ul>
+			<nav>
+				<ul class="navbar-right">
+					<li><a href="#" class="current">Home</a></li>
+					<li><a href="#">Size</a></li>
+					<li><a href="#">Feature</a></li>
+					<li><a href="#">Carts</a></li>
+					<li><a href="#">01</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<section class="banner">
+			<div class="banner-inner">
+				<img src="img/Flafs.png">
+			</div>
+		</section>
+	</div>
+	
+	<div class="content-2">
+		<h4>Our Offer</h4>
+		<p>Our Products are the best produts of its kind, it is reliable, affordable and could last a a lifetime.</p>
+
+		// list
+		<ul>
+			<li>
+				- First Product
+				<div class="content-list-1">
+					<div class="content-list-1-left">
+						<img src="img/pot.png">
+					</div>
+					<div class="content-list-1-right">
+						<ul>
+							<li>Waterproof 100%</li>
+							<li>Built in vacuum machine</li>
+							<li>Scratch resistance</li>
+							<li>Build to last</li>
+						</ul>
+					</div>
+				</div>
+			</li>
+
+			<li>
+				- Best Products
+				<div class="content-list-2">
+					<div class="content-list-2-left">
+						<img src="img/pot.png">
+					</div>
+					<div class="content-list-2-right">
+						<ul>
+							<li>Waterproof 100%</li>
+							<li>Built in vacuum machine</li>
+							<li>Scratch resistance</li>
+							<li>Build to last</li>
+						</ul>
+					</div>
+				</div>
+			</li>
+
+			<li>
+				- Affordable planning
+				<div class="content-list-3">
+					<div class="content-list-3-left">
+						<img src="img/pot.png">
+					</div>
+					<div class="content-list-3-right">
+						<ul>
+							<li>Waterproof 100%</li>
+							<li>Built in vacuum machine</li>
+							<li>Scratch resistance</li>
+							<li>Build to last</li>
+						</ul>
+					</div>
+				</div>
+			</li>
+
+		</ul>
+	</div>
+
+	<div class="content-3">
+		<img src="img/people.png">
+		
+		<div class="content-3-title">
+			<h2>Hello i am a title</h2>
+		</div>
+
+		<div class="content-3-content">
+			<div class="content-3-content-left">
+				
+			</div>
+			<div class="content-3-content-right">
+				
+			</div>
+
+			<div class="content-3-content-middle">
+				<h3>Why Us</h3>
+			</div>
+		</div>
+	</div>
+
+	<div class="content-4">
+		<div class="content-4-left">
+			<h1>Need a question</h1>
+		</div>
+		<div class="content-4-right">
+			<h1>Ask a question</h1>
+		</div>
+
+		<ul>
+			<li>Call us at </li>
+			<li>Phone us at </li>
+			<li>Fax us at</li>
+			<li>Open 8 hours a day, Monday-Friday</li>
+		</ul>
+	</div>
+
+	<div class="footer">
+		<ul>
+			<li>Materials</li>
+			<li>Affiliate</li>
+			<li>Contact Us</li>
+		</ul>
+
+		<ul>
+			<li>@ 2018 javaplanet17@gmail.com</li>
+			<li>By Jakso D.</li>
+		</ul>
+	</div>
+</body>
+</html>
