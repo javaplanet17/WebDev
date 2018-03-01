04454d81e2155f70b179f522cf113ff3796e58eb
Alan Dewo <alan@Alans-MacBook-Air.local>
2018-03-01 13:51:38 -0500

    daily update

--- website_6/index.php
+++ website_6/index.php
@@ -9,28 +9,36 @@
 <body>
 	<div class="wrapper">
 		<header>
-				<ul>
-					<li><img src="img/icon.png" width="30px" height="10px"></li>
-					<li><a href="index.php">Creditor</a></li>
-				</ul>
-			<nav>
-				<ul>
-					<li><a href="benefit.html">Benefit</a></li>
-					<li><a href="about.html">About</a></li>
-					<li><a href="start.html">Get started</a></li>
-					<li><a href="membership.php">Account</a></li>
-				</ul>	
-			</nav>
+			<div class="inner">
+				<div class="left">
+					<ul>
+						<li><img src="img/icon.png" width="30px" height="10px"></li>
+						<li><a href="index.php">Creditor</a></li>
+					</ul>
+				</div>
+					
+				<div class="right">
+					<nav>
+						<ul>
+							<li><a href="benefit.html">Benefit</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="start.html">Get started</a></li>
+							<li><a href="membership.php">Account</a></li>
+						</ul>	
+					</nav> 
+				</div>
+				
+			</div>
 		</header>
 
 		<section class="banner">
 			<div class="inner">
 				<div class="left">
-					<img src="img/banner.png">
+					<h1>Credit Card Hero</h1>
+					<p>oifhaiod ldhfl sdgl sdg lisdil sdl idhsl slghas lghsoi ghewo ihgalh goashg oag reoigeao gri gaer hiae</p>
 				</div>
 				<div class="right">
-					<h3>Credit Card Hero</h3>
-					<p>oifhaiod ldhfl sdgl sdg lisdil sdl idhsl slghas lghsoi ghewo ihgalh goashg oag reoigeao gri gaer hiae</p>
+					<img src="img/banner.png" width="300px" height="300px">
 				</div>
 			</div>
 		</section>
@@ -85,38 +93,54 @@
 			</div>
 		</section>
 
-		<section class="pre-requisite">
+		<section class="requisite">
 			<div class="inner">
 				<div class="top">
 					<h2>What is the benefit in applying credit card with us?</h2>
 				</div>
-				<div class="left">
-					
+				<div class="middle">
+					<div class="left">
+						<p><b>igni gsdig lsdig idslg ldsg ldsng </b></p>
+						<p>oinfdios ngois dng idsn gdsn glsdng sdg isdg oisdg oisd gsdg o sdg jsdg sdiog sdiog sdg dsg bsdgb sdbg usdb gsdg isdbg sud bsd gdksg bksdgb ksd </p>
+					</div>
+					<div class="right">
+						<ul>
+							<li>
+								<div class="requisite-list">
+									<img src="img/house.png">
+									<p><b>Closer to your dream home</b></p>
+									<p>it will get your closer to a good credit that can empower you to have your dream house.</p>
+								</div>
+								
+							</li>
+							<li>
+								<div class="requisite-list">
+									<img src="img/bussiness.png">
+									<p><b>Closer to your dream home</b></p>
+									<p>it will get your closer to a good credit that can empower you to have your dream house.</p>
+								</div>
+								
+							</li>
+							<li>
+								<div class="requisite-list">
+									<img src="img/investment.png">
+									<p><b>Closer to your dream home</b></p>
+									<p>it will get your closer to a good credit that can empower you to have your dream house.</p>
+								</div>
+								
+							</li>
+							<li>
+								<div class="requisite-list">
+									<img src="img/medical.png">
+									<p><b>Closer to your dream home</b></p>
+									<p>it will get your closer to a good credit that can empower you to have your dream house.</p>
+								</div>
+								
+							</li>
+						</ul>
+					</div>
 				</div>
-				<div class="right">
-					<ul>
-						<li>
-							<img src="img/house.png">
-							<p><b>Closer to your dream home</b></p>
-							<p>it will get your closer to a good credit that can empower you to have your dream house.</p>
-						</li>
-						<li>
-							<img src="img/business.png">
-							<p><b>Closer to your dream home</b></p>
-							<p>it will get your closer to a good credit that can empower you to have your dream house.</p>
-						</li>
-						<li>
-							<img src="img/investor.png">
-							<p><b>Closer to your dream home</b></p>
-							<p>it will get your closer to a good credit that can empower you to have your dream house.</p>
-						</li>
-						<li>
-							<img src="img/medical.png">
-							<p><b>Closer to your dream home</b></p>
-							<p>it will get your closer to a good credit that can empower you to have your dream house.</p>
-						</li>
-					</ul>
-				</div>
+				
 				<div class="bottom">
 					<img src="img/house.png">
 				</div>
@@ -125,7 +149,7 @@
 
 		<section class="start">
 			<div class="inner">
-				<div class="middle">
+				<div class="center">
 					<img src="img/tree.png">
 					<h2>Lets get you start up now.</h2>
 					<p>you will only need your current debit card or bank account. And you will need your goverment identification.</p>
@@ -135,36 +159,39 @@
 		</section>
 
 		<footer>
-			<div class="left">
-				<div class="top">
-					<ul>
-						<li>About</li>
-						<li>Career</li>
-						<li>Investment</li>
-						<li>Contact</li>
-						<li>Policy</li>
-					</ul>
+			<div class="inner">
+				<div class="left">
+					<div class="top">
+						<ul>
+							<li>About</li>
+							<li>Career</li>
+							<li>Investment</li>
+							<li>Contact</li>
+							<li>Policy</li>
+						</ul>
+					</div>
+					<div class="bottom">
+						<p>Our goal is to provide an easy access for an individual to a better experience at life, no matter their origin are. we are global companies that provide credit for everyone around the world.</p>
+					</div>
 				</div>
-				<div class="bottom">
-					<p>Our goal is to provide an easy access for an individual to a better experience at life, no matter their origin are. we are global companies that provide credit for everyone around the world.</p>
+				<div class="right">
+					<div class="top">
+						<ul>
+							<li><img src="img/icon.png"></li>
+							<li><p>Credit card hero</p></li>
+						</ul>
+					</div>
+					<div class="bottom">
+						<ul>
+							<li><img src="img/facebook.png"></li>
+							<li><img src="img/twitter.png"></li>
+							<li><img src="img/instagram.png"></li>
+							<li><img src="img/rss.png"></li>
+						</ul>
+					</div>
 				</div>
 			</div>
-			<div class="right">
-				<div class="top">
-					<ul>
-						<li><img src="img/icon.png"></li>
-						<li><p>Credit card hero</p></li>
-					</ul>
-				</div>
-				<div class="bottom">
-					<ul>
-						<li><img src="img/facebook.png"></li>
-						<li><img src="img/twitter.png"></li>
-						<li><img src="img/instagram.png"></li>
-						<li><img src="img/rss.png"></li>
-					</ul>
-				</div>
-			</div>
+			
 		</footer>
 	</div>
 </body>
